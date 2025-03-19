99b0607ab551251435cab3a2cf26638f9ee66555
Lewis Bull <lewisbull2303@gmail.com>
2025-03-19 09:10:45 +0000

    Added the basics to my home app, such as an index.html, a urls.py and added the views to render the index.html

--- home/views.py
+++ home/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+#Function to display home page
+
+def home(request):
+    """
+    A view to show the home page
+    """
+    return render(request, "home/index.html")
