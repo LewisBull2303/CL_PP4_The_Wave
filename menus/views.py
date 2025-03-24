b0df74f43ce83472ba03b5513a0a342f6bd503a9
Lewis Bull <lewisbull2303@gmail.com>
2025-03-24 14:47:21 +0000

    Added my drinks and food menu to my view

--- menus/views.py
+++ menus/views.py
@@ -1,3 +1,26 @@
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
 from django.shortcuts import render
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Internal:
+from .models import FoodItem, DrinkItem
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# Create your views here.
+
+def food_menu(request):
+    """
+    a view to display the food menu
+    """
+    food_list = FoodItem.objects.all()
+    return render(
+        request, 'menus/food_menu.html', {'food_list': food_list})
+
+
+def drink_menu(request):
+    """
+    a view to display the drinks menu
+    """
+    drink_list = DrinkItem.objects.all()
+    return render(
+        request, 'menus/drinks_menu.html', {'drink_list': drink_list})
