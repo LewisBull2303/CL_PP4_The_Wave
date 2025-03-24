b880f7d1c1a3a358c2d93160e9327943155a34be
Lewis Bull <lewisbull2303@gmail.com>
2025-03-24 14:50:33 +0000

    Added my admin registers to my menus to allow the admin to make a new item

--- menus/admin.py
+++ menus/admin.py
@@ -1,3 +1,29 @@
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Internal:
+from .models import FoodItem, DrinkItem
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# Register your models here.
+
+# Registration of the food items for the admin panel,
+# display and search filters
+@admin.register(FoodItem)
+class FoodAdmin(SummernoteModelAdmin):
+    list_display = ('food_name', 'food_type', 'price', 'available')
+    search_fields = ('food_name', 'description')
+    list_filter = ('available', 'food_type')
+    summernote_fields = ('description')
+
+
+# Registration of the drinks items for the admin panel,
+# display and search filters
+@admin.register(DrinkItem)
+class DrinkAdmin(SummernoteModelAdmin):
+    list_display = ('drink_name', 'drink_type', 'price', 'available')
+    search_fields = ('drink_name', 'description')
+    list_filter = ('available', 'drink_type')
+    summernote_fields = ('description')
