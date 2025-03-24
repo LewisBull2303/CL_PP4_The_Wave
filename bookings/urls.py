f87da5f28a7d98d3bde48cbdaaa18eb644766071
Lewis Bull <lewisbull2303@gmail.com>
2025-03-24 17:04:46 +0000

    Added the blog app and created the blog template

--- bookings/urls.py
+++ bookings/urls.py
@@ -9,7 +9,7 @@ from bookings import views
 # Urls for all the pages in the bookings app
 urlpatterns = [
     path('reservations', views.Reservations.as_view(), name='reservations'),
-    path('confirmed', views.Confirmed.as_view(), name='confirmed'),
+    path('confirmed_booking', views.Confirmed.as_view(), name='confirmed_booking'),
     path('booking_list', views.BookingList.as_view(), name='booking_list'),
     path('edit_booking/<int:pk>',
          views.EditBooking.as_view(), name='edit_booking'),
