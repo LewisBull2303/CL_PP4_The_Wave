60bcc983eed2bd9bfe7cf8c5f2c797d3ff826f71
Lewis Bull <lewisbull2303@gmail.com>
2025-03-28 12:30:41 +0000

    Allowed amIResponsive to see the webpage

--- the_wave/settings.py
+++ the_wave/settings.py
@@ -17,6 +17,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+X_FRAME_OPTIONS = 'ALLOW-FROM https://ui.dev/amiresponsive'
 
 ALLOWED_HOSTS = ['127.0.0.1', 'cl-pp4-the-wave-76e93eea9754.herokuapp.com']
 
